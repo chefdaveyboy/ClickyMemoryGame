9b51430a0d655db432e56738c97e8f0f55b3b6a1
David Koeller <david.koeller@gmail.com>
2020-04-21 22:11:43 -0500

    Updates

---
+++ precache-manifest.6d87a3abd78faf608aa9e6accd241ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ca17f6931b27042ec2a16d2c00d785",
+    "url": "/ClickyMemoryGame/index.html"
+  },
+  {
+    "revision": "599fb07488c18a71766f",
+    "url": "/ClickyMemoryGame/static/css/main.e8ede967.chunk.css"
+  },
+  {
+    "revision": "b12055fe2fc9e79d158f",
+    "url": "/ClickyMemoryGame/static/js/2.5ab82184.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyMemoryGame/static/js/2.5ab82184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599fb07488c18a71766f",
+    "url": "/ClickyMemoryGame/static/js/main.df38d36e.chunk.js"
+  },
+  {
+    "revision": "96071c5bdade3de41c17",
+    "url": "/ClickyMemoryGame/static/js/runtime-main.2e7dab83.js"
+  }
+]);
